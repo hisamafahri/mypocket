71da5280f1a789cc0cdc3b07950a0a22e15381f7
Hisam Fahri <hisam.fahri@pitik.id>
2023-12-07 08:23:21 +0700

    fix: session delete

--- src/app/auth/callback/session/delete/route.ts
+++ src/app/auth/callback/session/delete/route.ts
@@ -3,8 +3,10 @@ import { redirect } from "next/navigation";
 
 // eslint-disable-next-line import/prefer-default-export
 export const GET = async (request: Request) => {
-  const domain = new URL(process.env.NEXT_PUBLIC_APP_HOST || request.url)
-    .hostname;
+  const hostUrl = new URL(process.env.NEXT_PUBLIC_APP_HOST || request.url);
+  const hostnameParts = hostUrl.hostname.split(".");
+  const domain = hostnameParts.slice(-2).join(".");
+
   cookies().delete({ name: "username", domain });
   cookies().delete({ name: "access_token", domain });
 
