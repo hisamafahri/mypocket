688d962e3d66c9d787b3f88e66f953666649612e
Hisam Fahri <hisam.fahri@pitik.id>
2024-01-01 07:32:00 +0700

    fix: handle empty list

--- src/app/dashboard/_components/recordsList.tsx
+++ src/app/dashboard/_components/recordsList.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-
 "use client";
 
 import ContentCard from "./contentCard";
@@ -13,27 +11,29 @@ type RecordsListProps = {
 const RecordsList = ({ page }: RecordsListProps) => {
   const data = getRecordsFromLocalStorage().filter((record) => {
     if (page === "favorite") {
-      return record.favorite === "1" && record.status === "0"
+      return record.favorite === "1" && record.status === "0";
     }
 
     if (page === "archive") {
-      return record.status === "1"
+      return record.status === "1";
     }
 
     if (page === "list") {
-      return record.status === "0" && record.favorite === "0"
+      return record.status === "0" && record.favorite === "0";
     }
 
-    return record
+    return record;
   });
 
   return (
     <div className="bg-white rounded-lg border border-gray-200 h-full overflow-y-auto pb-24 p-4 w-full">
-      <SearchRecordDialog data={data} />
-      {data.map((record) => (
-        <ContentCard data={record} key={record.item_id} page={page} />
-      ))}
-      {!data.length && (
+      <SearchRecordDialog data={data || []} />
+      {data.length
+        ? data.map((record) => (
+            <ContentCard data={record} key={record.item_id} page={page} />
+          ))
+        : null}
+      {!data.length ? (
         <div className="flex flex-col items-center justify-center mt-24">
           <h3 className="font-semibold text-lg text-slate-700">
             Your pocket is empty!
@@ -42,7 +42,7 @@ const RecordsList = ({ page }: RecordsListProps) => {
             Begin saving your fantastic internet finds.
           </p>
         </div>
-      )}
+      ) : null}
     </div>
   );
 };
