a80d40cbf66def630a5275b6d2550e9ee5489d21
Hisam Fahri <me@hisamafahri.com>
2024-03-05 17:05:52 +0700

    fix: add missing import

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,15 +1,15 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { hoursToSeconds } from "date-fns";
-import { postGetAccessToken } from "../../../lib/services/api/authorization/server";
 import { DOMAINS, DUMMY_VALUES } from "../../../lib/utils/constants";
+import { postGetAccessTokenNoErrorCheck } from "../../../lib/services/api/authorization/server";
 
 // eslint-disable-next-line import/prefer-default-export
 export const GET = async (request: Request) => {
   const url = new URL(request.url);
   const code = url.searchParams.get("request_token") || DUMMY_VALUES.UUID;
   try {
-    const token = await postGetAccessToken({
+    const token = await postGetAccessTokenNoErrorCheck({
       body: {
         code,
       },
